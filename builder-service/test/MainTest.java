c32f3eece295d0decfe3286466249ba13a8a74e8
Octavio Roncal <octavioroncal11@gmail.com>
2024-10-21 10:01:39 +0100

    fixed test

--- builder-service/test/MainTest.java
+++ builder-service/test/MainTest.java
@@ -1,10 +1,9 @@
 import io.intino.builderservice.konos.BuilderServiceBox;
 import io.intino.builderservice.konos.actions.PostBuildersAction;
-import io.intino.builderservice.konos.schemas.BuilderInfo;
+import io.intino.builderservice.konos.schemas.RegisterBuilder;
 import org.apache.commons.io.FileUtils;
 
 import java.io.IOException;
-import java.util.List;
 
 public class MainTest {
 
@@ -19,10 +18,8 @@ public class MainTest {
 
 		var action = new PostBuildersAction();
 		action.box = box;
-		action.builderInfo = new BuilderInfo()
-				.id("io.intino.tara.builder:1.3.0")
-				.imageName("octavioroncal/io.intino.tara.builder:1.3.0")
-				.operations(List.of("Build"));
+		action.info = new RegisterBuilder()
+				.imageURL("octavioroncal/io.intino.tara.builder:1.3.0");
 		action.execute();
 		System.out.println("DONE");
 	}
