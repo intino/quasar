a65ea8f91852a5be3e84a9f27207416c5d041f24
Octavio Roncal <octavioroncal@monentia.com>
2025-05-02 10:23:56 +0100

    updating dependencies

--- builder-sdk/test/DummyModelOperation.java
+++ builder-sdk/test/DummyModelOperation.java
@@ -1,8 +1,8 @@
 import io.intino.tara.builder.core.CompilationUnit;
 import io.intino.tara.builder.core.errorcollection.CompilationFailedException;
 import io.intino.tara.builder.core.operation.model.ModelOperation;
-import io.intino.tara.builder.model.Model;
-import io.intino.tara.language.model.Mogram;
+import io.intino.tara.model.Mogram;
+import io.intino.tara.processors.model.Model;
 
 public class DummyModelOperation extends ModelOperation {
 
