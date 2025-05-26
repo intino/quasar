9cdd90d1a720724304d11e142466478796948b87
Octavio Roncal <octavioroncal@monentia.com>
2025-05-26 10:12:16 +0100

    improved ModelReader.itr

--- quassar-builder/test/BuilderRunnerUseCaseTest.java
+++ quassar-builder/test/BuilderRunnerUseCaseTest.java
@@ -32,7 +32,7 @@ public class BuilderRunnerUseCaseTest {
 	@Test
 	@Ignore
 	public void should_run_quassar_builder_with_picota_m2() {
-		QuassarcRunner.main(new String[]{"/Users/oroncal/workspace/infrastructure/quassar/temp/builder-service/workspace/picota1/tara_args.txt"});
+		QuassarcRunner.main(new String[]{"/Users/oroncal/workspace/infrastructure/quassar/temp/builder-service/workspace/garaxia/tara_args.txt"});
 	}
 
 	@Test
